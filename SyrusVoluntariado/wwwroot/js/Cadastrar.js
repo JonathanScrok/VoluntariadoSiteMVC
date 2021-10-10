93d25ddfe13de7c5b9a9e6032f80a65a0fc4b117
Jonathan Scrok <jonathanwilliamscrok@gmail.com>
2021-10-10 10:55:21 -0300

    Não deixando escolher os dois checkbox

--- SyrusVoluntariado/wwwroot/js/Cadastrar.js
+++ SyrusVoluntariado/wwwroot/js/Cadastrar.js
@@ -1,7 +1,11 @@
 ﻿function validaCheckSemData() {
     var chkSemData = document.getElementById('SemData');
+    var chkEventoRecorrente = document.getElementById('EventoRecorrente');
+
     if (chkSemData.checked) {
         document.getElementById("SemData").value = true;
+        chkEventoRecorrente.checked = false;
+        document.getElementById("EventoRecorrente").value = false;
     }
     else {
         document.getElementById("SemData").value = false;
@@ -10,8 +14,12 @@
 
 function validaCheckEventoRecorrente() {
     var chkEventoRecorrente = document.getElementById('EventoRecorrente');
+    var chkSemData = document.getElementById('SemData');
+
     if (chkEventoRecorrente.checked) {
         document.getElementById("EventoRecorrente").value = true;
+        chkSemData.checked = false;
+        document.getElementById("SemData").value = false;
     }
     else {
         document.getElementById("EventoRecorrente").value = false;
