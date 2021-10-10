d59d7d764c3b6971ce296d7d697fcbb3209f17b8
Jonathan Scrok <jonathanwilliamscrok@gmail.com>
2021-10-10 10:47:02 -0300

    Migração de script para o arquivo js

---
+++ SyrusVoluntariado/wwwroot/js/Cadastrar.js
@@ -0,0 +1,19 @@
+﻿function validaCheckSemData() {
+    var chkSemData = document.getElementById('SemData');
+    if (chkSemData.checked) {
+        document.getElementById("SemData").value = true;
+    }
+    else {
+        document.getElementById("SemData").value = false;
+    }
+}
+
+function validaCheckEventoRecorrente() {
+    var chkEventoRecorrente = document.getElementById('EventoRecorrente');
+    if (chkEventoRecorrente.checked) {
+        document.getElementById("EventoRecorrente").value = true;
+    }
+    else {
+        document.getElementById("EventoRecorrente").value = false;
+    }
+}
