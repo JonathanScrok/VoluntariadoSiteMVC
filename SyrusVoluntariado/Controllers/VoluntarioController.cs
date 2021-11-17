f69672a09fc57cd572bc71fe13551e1309808e4c
Jonathan Scrok <jonathanwilliamscrok@gmail.com>
2021-11-16 21:14:18 -0300

    Criação da lista de voluntários

---
+++ SyrusVoluntariado/Controllers/VoluntarioController.cs
@@ -0,0 +1,30 @@
+﻿using BeaHelper.BLL.BD;
+using BeaHelper.BLL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyrusVoluntariado.Controllers
+{
+    public class VoluntarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            var voluntarios = Usuario_P2.TodosUsuarios();
+
+            List<UsuarioCompleto> voluntariosCompleto = new List<UsuarioCompleto>();
+            foreach (var voluntario in voluntarios)
+            {
+                var AvaliacaoUsuario = Avaliacao_P1.TodasAvaliacoesUsuario(voluntario.Id);
+            }
+
+            var pageNumber = 1;
+            //var resultadoPaginado = voluntariosCompleto.ToPagedList(pageNumber, 10);
+
+            UsuarioCompleto UsuarioCompleto = new UsuarioCompleto();
+            return View(UsuarioCompleto);
+        }
+    }
+}
