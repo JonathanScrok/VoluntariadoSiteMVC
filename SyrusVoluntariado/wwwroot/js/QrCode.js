6f6ddd2d32b77cf5e6a4c45c91b97e5761c4ad1a
Jonathan Scrok <jonathanwilliamscrok@gmail.com>
2022-01-11 21:59:11 -0300

    Adicionando opção de imprimir QrCode

---
+++ SyrusVoluntariado/wwwroot/js/QrCode.js
@@ -0,0 +1,22 @@
+﻿function imprimir() {
+
+        // Remove Menu topo
+        $("#navbarId").hide();
+        $("#logoSiteBAH").hide();
+        $("#navbarOptions").hide();
+        $(".FoooterSite").hide();
+        $(".botaoImprimir").hide();
+        //$('body').css({ "padding-top": "0px" });
+
+        // Imprime a página
+        window.print();
+
+        location.reload();
+
+        // Adiciona Menu topo
+        //$("#navbarId").show();
+        //$("#logoSiteBAH").show();
+        //$(".FoooterSite").show();
+        //$(".botaoImprimir").show();
+        //$('body').css({ "padding-top": "40px" });
+    }
