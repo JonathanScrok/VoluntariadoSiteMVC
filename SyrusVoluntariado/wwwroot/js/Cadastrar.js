2c7c86a3d74052aec6e826ca3f2119083503477e
Jonathan Scrok <jonathanwilliamscrok@gmail.com>
2022-06-08 20:28:49 -0300

    Adicionando Autocomplete

--- SyrusVoluntariado/wwwroot/js/Cadastrar.js
+++ SyrusVoluntariado/wwwroot/js/Cadastrar.js
@@ -25,3 +25,32 @@ function validaCheckEventoRecorrente() {
         document.getElementById("EventoRecorrente").value = false;
     }
 }
+
+function autoComplete(cidade) {
+
+    const destino = ['Salvador', 'Vitória', 'Maceió', 'Ceará', 'Rio Branco', 'Macapá', 'Porto Velho', 'Olinda', 'Aracaju', 'Capitólio', 'São Paulo', 'Paraty'];
+    return destino.filter((valor) => {
+        const valorMinusculo = valor.toLowerCase()
+        const cidadeMinusculo = cidade.toLowerCase()
+
+        return valorMinusculo.includes(cidadeMinusculo)
+    })
+}
+const campo = document.querySelector('#Cidade_Estado')
+const sugestoes = document.querySelector('.sugestoes')
+
+campo.addEventListener('input', ({ target }) => {
+    const dadosDoCampo = target.value
+    if (dadosDoCampo.length) {
+        const autoCompleteValores = autoComplete(dadosDoCampo)
+        sugestoes.innerHTML = `
+            ${autoCompleteValores.map((value) => {
+                return (`<li><a href="#" onclick="valorEscolhido('${value}')" style="color: black;">${value}</a></li>`)
+            }).join('')}`
+    }
+})
+
+function valorEscolhido(valor) {
+    document.querySelector('#Cidade_Estado').value = valor;
+    document.querySelector('.sugestoes').innerHTML = '';
+}
